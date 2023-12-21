7bfee21d3a15923caf50e7bf74c40b756fca19fd
nadaMAoui <maaouinada747@gmail.com>
2023-12-21 12:27:20 +0100

    narkdown2html

---
+++ markdown2html.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""markdown2html"""
+
+import sys
+import os
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
+        sys.exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        sys.stderr.write(f"Missing {sys.arg[1]} \n")
+        sys.exit(1)
+
+
