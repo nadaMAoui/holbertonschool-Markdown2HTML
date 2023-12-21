f95a63dddf128080e1033aa75d737c3578bb41f0
nadaMAoui <maaouinada747@gmail.com>
2023-12-21 15:17:13 +0100

    markdown2html

--- markdown2html.py
+++ markdown2html.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""markdown2html"""
+"""covert .md file to html one"""
 
 import sys
 import os
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
     if not os.path.isfile(sys.argv[1]):
         sys.stderr.write(f"Missing {sys.argv[1]}\n")
         sys.exit(1)
+
     with open(sys.argv[1], 'r') as file:
         lines = file.readlines()
     with open(sys.argv[1], 'w') as file:
