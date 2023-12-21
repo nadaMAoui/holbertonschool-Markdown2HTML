c9dfd432d9ca0e5c1f598c2f58853cfdc70ad683
nadaMAoui <maaouinada747@gmail.com>
2023-12-21 12:31:34 +0100

    narkdown2html

--- markdown2html.py
+++ markdown2html.py
@@ -7,11 +7,10 @@ import os
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
     if not os.path.isfile(sys.argv[1]):
-        sys.stderr.write(f"Missing {sys.arg[1]} \n")
+        sys.stderr.write(f"Missing {sys.argv[1]} \n")
         sys.exit(1)
-
 
